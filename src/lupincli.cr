43392359311998d538a44f34bf914353e6c9936d
Mark Molnar <molnarpw@gmail.com>
2018-09-11 19:39:38 +0200

    Help message added

--- src/lupincli.cr
+++ src/lupincli.cr
@@ -1,4 +1,5 @@
 require "bargs"
+require "colorize"
 require "./lupincli/*"
 require "./lupincli/initializers/*"
 
@@ -7,32 +8,33 @@ module LupinCli
   LUPINFILE_PATH = "./lupinfile.cr"
 
   def self.init(args)
-    mode = "cr"
-    if args.size > 0
-      mode = args[0]
-    end
-
-    case mode
-    when "cr"
-      CrystalInitializer.new
-    end
+    CrystalInitializer.new
   end
 
   def self.run(args)
     if args.size == 0
-      # Run task "default"
       puts "Running default task"
+    elsif args.size == 1
+      puts "running task #{args[0]}"
     else
       puts "Running tasks: #{args.to_s}"
     end
   end
 
-  interface = Bargs::CLI.new
-  interface.help = "Lupin CLI 1.0"
+  lupinfile_ref = "lupinfile.cr".colorize(:yellow)
+  help_message = "#{"Lupin CLI #{VERSION}".colorize(:yellow).mode(:underline).mode(:bold).to_s}
 
-  interface.flag "version" do |flag|
-    flag.short = "s"
-  end
+#{"Available commands:".colorize(:blue).mode(:bold).to_s}
+  • init - Initializes an empty #{lupinfile_ref} inside the current directory.
+  • run - Runs the default task in the current directory.
+  • run #{"<task>".colorize(:blue).to_s} - Runs the given task in the current directory.
+  • version - Outputs the version of #{"Lupin CLI".colorize(:yellow).to_s}.
+  • help - Shows this help message.
+
+  #{"Thank you for using Lupin!".colorize(:yellow).mode(:bold).to_s}
+  "
+  interface = Bargs::CLI.new
+  interface.help = help_message
 
   args = interface.process
 
@@ -41,7 +43,9 @@ module LupinCli
     self.init(args.rest)
   when "run"
     self.run(args.rest)
-  when "-v", "--version"
-    puts "Lupin CLI v#{VERSION}"
+  when "version"
+    puts "Lupin CLI #{VERSION}"
+  when "help"
+    puts help_message
   end
 end
--- src/lupincli/initializers/crystal.cr
+++ src/lupincli/initializers/crystal.cr
@@ -1,23 +1,25 @@
+require "epilog"
+
 module LupinCli
   class CrystalInitializer
     TEMPLATE = "require \"lupin\"
 
-  # Create our tasks.
-  Lupin.task(\"template\")
+# Create your tasks here!
 
+Lupin.task(\"first-step\")
+  .command(\"touch lupin-works.txt\")
     "
 
     def initialize
-      # TODO use Epilog
+      @logger = Epilog::Logger.new
       if File.exists?(LUPINFILE_PATH)
-        puts("lupinfile.cr already exists in target directory.")
+        @logger.fatal("lupinfile.cr already exists in target directory.")
         Process.exit(0)
       else
-        # Create our lupinfile
         File.write(LUPINFILE_PATH, TEMPLATE)
       end
-      # TODO use Epilog
-      puts "Created lupinfile.cr in target directory."
+      @logger.success("Created lupinfile.cr in target directory.")
+      @logger.ready("Have fun using Lupin!")
     end
   end
 end
